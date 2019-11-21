146eb3b28ef4c72d795a2c5465272c037666bb6d
viethungbk <anhhung021098@gmail.com>
2019-11-21 14:36:45 +0700

    feature: add server

---
+++ db/mongoose.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('error', err => {
+  console.error('MongoDB connection error.');
+  console.error(err);
+  process.exit();
+});
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connect successfully');
+});
