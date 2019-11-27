dc29d8d1dc58b67c82e807c25972674d25ec29ac
viethungbk <anhhung021098@gmail.com>
2019-11-27 12:03:54 +0700

    feat: update add recipe material

---
+++ db/material.db.js
@@ -0,0 +1,32 @@
+const Material = require('../models/material.model')
+
+const findRecipeMaterial = async (idRecipe) => {
+  const rs = await Material.findOne({ idRecipe: idRecipe })
+  return rs
+}
+
+const addMaterialsDb = async (data) => {
+  const { idRecipe, materials } = data
+  const foundMaterial = await findRecipeMaterial(idRecipe)
+  console.log(materials)
+
+  if (!foundMaterial) {
+    const newMaterial = new Material({
+      idRecipe,
+      materials
+    })
+    console.log('new')
+    const rs = await newMaterial.save()
+    return rs
+  } else {
+    console.log('founds')
+    console.log(foundMaterial.materials)
+    foundMaterial.materials = foundMaterial.materials.concat(materials)
+    const rs = await foundMaterial.save()
+    return rs
+  }
+}
+
+module.exports = {
+  addMaterialsDb
+}
