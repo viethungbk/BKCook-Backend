75cc6b96c535f16e24f6df30be86313964279adb
viethungbk <anhhung021098@gmail.com>
2019-12-08 16:13:11 +0700

    feat: update server port and db url

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const cors = require('cors')
 const path = require('path')
 
 const app = express()
-const port = process.env.PORT || 5000
+// const port = process.env.PORT || 5000
+const port = 2900
 const errorHandler = require('./middlewares/errorHandler')
 
 app.use(express.json())
